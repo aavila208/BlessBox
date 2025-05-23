73d935bd722a77ca4e958f066a61bf2f469c9ab3
Alexander <aavila208@outlook.com>
2025-05-23 17:50:15 -0500

    Fix: add comment field to order schema correctly

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -6,8 +6,8 @@ const orderSchema = new mongoose.Schema({
     address:{type:Object,required:true},
     status: {type:String,default:"Food Processing"},
     date: {type:Date,default:Date.now()},
-    payment:{type:Boolean,default:false}
-    comment: {type:String,default:""}, // [First]
+    payment:{type:Boolean,default:false},
+    comment: {type:String,default:""} // [First]
 })
 
 const orderModel = mongoose.models.order || mongoose.model("order", orderSchema);
