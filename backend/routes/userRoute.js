b2dbfb63c8af6d0a8d77362a5d281e57e8842693
Alexander <aavila208@outlook.com>
2025-05-24 02:01:30 -0500

    Added Users Table for Admin Panel

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,11 @@
 import express from 'express';
-import { loginUser,registerUser } from '../controllers/userController.js';
+import { loginUser,registerUser, listUsers} from '../controllers/userController.js';
+import authMiddleware from '../middleware/auth.js';
+import isAdmin from '../middleware/isAdmin.js';
+
 const userRouter = express.Router();
 
+userRouter.get("/list", authMiddleware, isAdmin, listUsers);
 userRouter.post("/register",registerUser);
 userRouter.post("/login",loginUser);
 
